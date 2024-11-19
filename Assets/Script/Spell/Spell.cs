8693114914151754be801201d27d6fb3b38770ea
devalexxx <46360108+alspin8@users.noreply.github.com>
2024-11-19 23:56:37 +0100

    Add to spell and spell instance the feature to trigger effects

--- Assets/Script/Spell/Spell.cs
+++ Assets/Script/Spell/Spell.cs
@@ -1,12 +1,26 @@
 using System;
+using UnityEngine;
 
+[Serializable]
 public class Spell
 {
-    private SpellBehaviour _behaviour;
+    [SerializeField] private SpellBehaviour _behaviour;
+
+    public SpellBehaviour Behaviour { get => _behaviour; }
 
     public Spell(SpellBehaviour behaviour)
     {
         _behaviour = behaviour;
     }
 
+    public bool Trigger(ChampionInstance self, ChampionInstance target, CrewInstance allies, CrewInstance enemies)
+    {
+        // @TODO: add effect revert in case of fail
+        bool hasFailed = false;
+        foreach (SpellEffect effect in _behaviour.Effects)
+        {
+            hasFailed |= effect.Apply(self, target, allies, enemies);
+        }
+        return hasFailed;
+    }
 }
