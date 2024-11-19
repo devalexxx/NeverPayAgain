ffcb458ef0dab75e8ba81d0861a49cc678bdc3c6
devalexxx <46360108+alspin8@users.noreply.github.com>
2024-11-19 23:56:37 +0100

    Add to spell and spell instance the feature to trigger effects

--- Assets/Script/Spell/SpellInstance.cs
+++ Assets/Script/Spell/SpellInstance.cs
@@ -8,4 +8,10 @@ public class SpellInstance
     {
         _spell = spell;
     }
+
+    public bool Trigger(ChampionInstance self, ChampionInstance target, CrewInstance allies, CrewInstance enemies)
+    {
+        // @TODO: Add turn counter check
+        return _spell.Trigger(self, target, allies, enemies);
+    }
 }
