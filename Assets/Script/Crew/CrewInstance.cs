3ce919e7ab6d53921aa826bd781e54f0507d8570
devalexxx <46360108+alspin8@users.noreply.github.com>
2024-11-21 23:49:34 +0100

    Feature: Add crew instance unique id to enable comparison + add method to check if a champion instance is in this crew instance

--- Assets/Script/Crew/CrewInstance.cs
+++ Assets/Script/Crew/CrewInstance.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using UnityEngine;
 
 [Serializable]
-public class CrewInstance
+public class CrewInstance : IEqualityComparer<CrewInstance>
 {
+    [SerializeField] private Guid _guid;
+
     [SerializeField] private List<ChampionInstance> _instances;
 
     public CrewInstance(Crew crew)
     {
+        _guid      = Guid.NewGuid();
         _instances = crew.Champions.Select(ch => new ChampionInstance(ch)).ToList();
     }
 
@@ -23,6 +26,21 @@ public class CrewInstance
 
     public bool IsAlive()
     {
-        return _instances.Sum(ch => ch.Health) > 0.0f;
+        return _instances.Any(inst => inst.Health > 0);
+    }
+
+    public bool Contains(ChampionInstance inst)
+    {
+        return _instances.Contains(inst);
+    }
+
+    public bool Equals(CrewInstance x, CrewInstance y)
+    {
+        return x._guid == y._guid;
+    }
+
+    public int GetHashCode(CrewInstance obj)
+    {
+        return obj._guid.GetHashCode();
     }
 }
