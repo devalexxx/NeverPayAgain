1f84fbe112e257e1a503bb24cee1893425240949
devalexxx <46360108+alspin8@users.noreply.github.com>
2024-11-22 00:41:15 +0100

    Feature: Add turn counter to spells

--- Assets/Script/Spell/SpellInstance.cs
+++ Assets/Script/Spell/SpellInstance.cs
@@ -1,17 +1,45 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
 
-
+[Serializable]
 public class SpellInstance
 {
-    private Spell _spell;
+    [SerializeField] private Spell _spell;
+    [SerializeField] private uint  _turnSinceEnable;
+
+    public Spell Spell
+    {
+        get => _spell;
+    }
+
+    public uint TurnSinceEnable
+    {
+        get => _turnSinceEnable;
+    }
 
     public SpellInstance(Spell spell)
     {
         _spell = spell;
+        _turnSinceEnable = 0;
     }
 
     public bool Trigger(ChampionInstance self, ChampionInstance target, CrewInstance allies, CrewInstance enemies)
     {
-        // @TODO: Add turn counter check
-        return _spell.Trigger(self, target, allies, enemies);
+        if (_turnSinceEnable == 0)
+        {
+            _turnSinceEnable = _spell.Behaviour.Cooldown;
+            return _spell.Trigger(self, target, allies, enemies);
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void OnTurn()
+    {
+        if (_turnSinceEnable > 0)
+            _turnSinceEnable -= 1   ;
     }
 }
