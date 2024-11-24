1614f4d8485a104da34e3e682520a79754d0ef8d
devalexxx <46360108+alspin8@users.noreply.github.com>
2024-11-24 17:29:55 +0100

    Feature: Add spell methods to update champion state depending to CrewTarget

--- Assets/Script/Spell/Spell.cs
+++ Assets/Script/Spell/Spell.cs
@@ -1,5 +1,7 @@
 using System;
 using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
 
 [Serializable]
 public class Spell
@@ -21,6 +23,46 @@ public class Spell
         {
             hasFailed |= effect.Apply(self, target, allies, enemies);
         }
+
         return hasFailed;
+    }
+
+    public void SetTargetState(CrewInstance allies, CrewInstance enemies)
+    {
+        switch (_behaviour.Target)
+        {
+            case SpellCrewTarget.Ally:
+                allies.ForEach(SetChampionState);
+                break;
+            case SpellCrewTarget.Enemy:
+                enemies.ForEach(SetChampionState);
+                break;
+        }
+    }
+
+    public void ResetTargetState(CrewInstance allies, CrewInstance enemies)
+    {
+        switch (_behaviour.Target)
+        {
+            case SpellCrewTarget.Ally:
+                allies.ForEach(ResetChampionState);
+                break;
+            case SpellCrewTarget.Enemy:
+                enemies.ForEach(ResetChampionState);
+                break;
+        }
+    }
+
+    private void SetChampionState(ChampionInstance inst)
+    {
+        inst.State = ChampionInstanceState.Target;
+    }
+
+    private void ResetChampionState(ChampionInstance inst)
+    {
+        if (inst.State == ChampionInstanceState.TurnAndTarget || inst.State == ChampionInstanceState.Turn)
+            inst.State = ChampionInstanceState.Turn;
+        else
+            inst.State = ChampionInstanceState.Idle;   
     }
 }
