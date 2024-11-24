290b8806af34c7051763201c0de901a779e6351c
devalexxx <46360108+alspin8@users.noreply.github.com>
2024-11-24 17:19:56 +0100

    Feature: Script that can be add to spell prefab by a spell instance to greyed a spell if it is in cooldown

---
+++ Assets/Script/Spell/SpellInstanceCooldownViewer.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class SpellInstanceCooldownViewer : MonoBehaviour
+{
+    [SerializeField] private SpellInstance _instance;
+
+    [SerializeField] private Image _image;
+
+    public SpellInstance Instance
+    {
+        get => _instance;
+        set => _instance = value;
+    }
+
+    private void Awake()
+    {
+        _image = transform.Find("Mask").Find("Image").GetComponent<Image>();
+    }
+
+    private void Update()
+    {
+        if (_instance.TurnSinceEnable > 0)
+        {
+            if (_image.color.a > 0.75)
+            {
+                var color    = _image.color;
+                color.a      = 0.75f;
+                _image.color = color;
+            }
+        }
+        else
+        {
+            if (_image.color.a < 1f)
+            {
+                var color    = _image.color;
+                color.a      = 1f;
+                _image.color = color;
+            }
+        }
+    }   
+}
